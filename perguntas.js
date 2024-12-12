f380c3e185df9af1cc88ffb71fa9ce7ad1efc9f6
Haikawa-nick <161317936+Haikawa-nick@users.noreply.github.com>
2024-12-12 16:09:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável em programação?',
+    'Uma variável é um espaço de memória utilizado para armazenar dados temporários.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o maior continente do mundo?',
+    'O maior continente do mundo é a Ásia.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequência de passos ou instruções para resolver um problema.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz obrigado em Inglês?',
+    'Obrigado em inglês é "Thank you".' 
+)
+
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula da área do triângulo?',
+    'A fórmula da área do triângulo é A = (base * altura) / 2.'
+)
+
+criaCartao(
+    'História',
+    'Em que ano foi proclamada a independência do Brasil?',
+    'A independência do Brasil foi proclamada em 1822.'
+)
